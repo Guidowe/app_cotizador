e2280c0e2770422396f3ec5ef2e2a6e4d231758c
Guido Weksler <39995669+Guidowe@users.noreply.github.com>
2025-09-01 12:45:53 -0300

    corrijo logo

--- src/stream_ok.py
+++ src/stream_ok.py
@@ -14,7 +14,7 @@ count = st_autorefresh(interval=refresh_interval_ms, limit=None, key="auto-refre
 
 col1, col2 = st.columns([1, 6])
 with col1:
-    st.image("src/img/logo_dgm.jpg",use_column_width=True ,heigth=80)
+    st.image("src/img/logo_dgm.jpg", width=80,heigth=80)
 with col2:
     st.markdown("<h1 style='margin-top: 25px;'>DGM - Quotation System</h1>", unsafe_allow_html=True)
 
